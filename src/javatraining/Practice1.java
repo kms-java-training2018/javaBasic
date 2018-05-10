160580fc085d9ec37bc0ce1495bbfb45cb8b886a
yamagishi-toko <yamagishi-toko@KMS2013-78.kms.local>
2018-05-10 10:11:15 +0900

    練習問題提出

---
+++ src/javatraining/Practice1.java
@@ -0,0 +1,24 @@
+package javatraining;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//1つ目の数値を入力
+		System.out.println("1つ目の数値を入力してください。");
+		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+
+
+		//2つ目の数値を入力
+
+
+
+
+		//文字列として連結し表示
+	}
+
+}
