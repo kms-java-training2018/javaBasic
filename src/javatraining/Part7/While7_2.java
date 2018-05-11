25f3fc014dbcc521170713a1f3c3c05bc0a63541
yamagishi-toko <yamagishi-toko@KMS2013-78.kms.local>
2018-05-11 14:29:47 +0900

    While7_2提出

--- src/javatraining/Part7/While7_2.java
+++ src/javatraining/Part7/While7_2.java
@@ -5,13 +5,6 @@ public class While7_2 {
     public static void main(String[] args) {
         // TODO 自動生成されたメソッド・スタブ
 
-        /*for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < i * i; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        } */
-
         int i = 0;
         int j = 0;
         while (i < 10) {
