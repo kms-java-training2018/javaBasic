b38884324f3a272f6ed3f7a176ef9bd1582dec6d
takatsuka-akihito <takatsuka-akihito@KMS2013-69.kms.local>
2018-05-11 13:03:56 +0900

    練習問題です。

---
+++ src/javatraining/For6_2.java
@@ -0,0 +1,16 @@
+package javatraining;
+
+public class For6_2 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i + ":");
+			for (int j = 0; j < i * i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
