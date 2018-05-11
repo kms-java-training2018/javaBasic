a8cbeb4cfcd133a126946b8d6fa54d0c2cf0bd46
hirachi-takahiro <hirachi-takahiro@KMS2013-92.kms.local>
2018-05-11 13:41:17 +0900

    6章　練習問題6-2提出

--- src/javatraining/Practice1.java
+++ src/javatraining/Practice1.java
@@ -5,18 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Practice1 {
-	public static void main(String[] args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			string line;
-			//1つめ
-			System.out.println("1つめの値を入力してください。");
-			String number1 = reader.readLine();
-			//2つめ
-			System.out.println("2つめの値を入力してください。");
-			String number2 = reader.readLine();
-		} catch (IOException e) {
-			System.out.println(e);
-		}
-	}
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            //1つめ
+            System.out.println("1つめの値を入力してください。");
+            String input1 = reader.readLine();
+            //2つめ
+            System.out.println("2つめの値を入力してください。");
+            String input2 = reader.readLine();
+            System.out.println(input1 + input2);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
