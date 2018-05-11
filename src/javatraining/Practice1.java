8a5a8247368749404d9fcc2f477bae30435f6072
iijima-naoyuki <iijima-naoyuki@KMS2014-06.kms.local>
2018-05-11 16:50:13 +0900

    Plactice1,2の修正

--- src/javatraining/Practice1.java
+++ src/javatraining/Practice1.java
@@ -6,21 +6,21 @@ import java.io.InputStreamReader;
 
 public class Practice1 {
 
-	public static void main(String[] args) {
-		System.out.println("一つ目の数値を入力してください。");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	    try {
-	    	String line = reader.readLine();  //一つ目の数値を取得
-	    	int a = Integer.parseInt(line);
-	    	System.out.println("二つ目の数値を入力してください。");
-	    	line =reader.readLine();  //二つ目の数値を取得
-	    	int b = Integer.parseInt(line);
-	    	System.out.println(a+""+b);  //数値の間に空の文字列を挟む
-	    } catch (IOException e) {
-	    	System.out.println(e);
-	    } catch (NumberFormatException e) {
-	    	System.out.println("数値が正しくありません。");
-	    }
-	}
+    public static void main(String[] args) {
+        System.out.println("一つ目の数値を入力してください。");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String line = reader.readLine();  //一つ目の数値を取得
+            int a = Integer.parseInt(line);
+            System.out.println("二つ目の数値を入力してください。");
+            line =reader.readLine();  //二つ目の数値を取得
+            int b = Integer.parseInt(line);
+            System.out.println(a+""+b);  //数値の間に空の文字列を挟む
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (NumberFormatException e) {
+            System.out.println("数値が正しくありません。");
+        }
+    }
 
 }
