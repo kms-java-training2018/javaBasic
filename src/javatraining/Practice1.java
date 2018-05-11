fd0bfa244527125f7259d65276d198b83976f8a4
takatsuka-akihito <takatsuka-akihito@KMS2013-69.kms.local>
2018-05-11 16:17:14 +0900

    FizzBuzzです。

--- src/javatraining/Practice1.java
+++ src/javatraining/Practice1.java
@@ -5,22 +5,22 @@ import java.io.InputStreamReader;
 
 public class Practice1 {
 
-	public static void main(String[] args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			try {
-				//1
-				System.out.println("数値を入力してください。");
-				String Number1 = reader.readLine();
-				//2
-				System.out.println("数値を入力してください。");
-				String Number2 = reader.readLine();
-				//足す
-				System.out.println(Number1 + Number2);
-			}
-		catch(Exception e) {
-			System.out.println(e);
-		}
-
-	}
-
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                //1
+                System.out.println("数値を入力してください。");
+                String Number1 = reader.readLine();
+                //2
+                System.out.println("数値を入力してください。");
+                String Number2 = reader.readLine();
+                //足す
+                System.out.println(Number1 + Number2);
+            }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+            
+    }
+    
 }
