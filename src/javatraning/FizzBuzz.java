9a132e4ea0a433131950a576e2bb89542fe7fcbe
onishi-yasumasa <onishi-yasumasa@KMS2013-110.kms.local>
2018-05-11 16:17:29 +0900

    課題③FizzBuzz 課題④数当てゲーム を提出します

---
+++ src/javatraning/FizzBuzz.java
@@ -0,0 +1,28 @@
+package javatraning;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//1から100までを出力するfor文
+		for(int i = 1;i <= 100;i++) {
+
+			//もし15の倍数ならFizzBuzzを表示
+			if (i % 15 == 0) {
+				System.out.println("FizzBuzz");
+			//もし5の倍数ならFizzを表示
+			}else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			//もし3の倍数ならFizzを表示
+			}else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			}else
+				System.out.println(i);
+
+		}
+
+
+	}
+
+}
