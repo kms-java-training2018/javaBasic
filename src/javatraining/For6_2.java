e95e4042520aab3065e1f711be70fdcf1bc9e71d
iyo-marina <iyo-marina@KMS2013-114.kms.local>
2018-05-11 13:02:59 +0900

    練習問題6-2

---
+++ src/javatraining/For6_2.java
@@ -0,0 +1,16 @@
+package javatraining;
+
+public class For6_2 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        int i = 0;
+        for ( i = 0 ; i < 10; i++) {
+                System.out.print(i + ":");
+                for (int j = 0; j < i*i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+    }
+}
