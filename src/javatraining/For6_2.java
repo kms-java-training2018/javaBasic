7e96d00c7da1a478f3870b024c22d94b48df85f3
ishii-masahiro <ishii-masahiro@KMS2013-113.kms.local>
2018-05-11 14:22:26 +0900

    While7_2を提出します。

--- src/javatraining/For6_2.java
+++ src/javatraining/For6_2.java
@@ -2,18 +2,18 @@ package javatraining;
 
 public class For6_2 {
 
-	public static void main(String[] args) {
-		// 段数の限度指定
-		int dan = 10;
-		// 段数表示のfor文開始
-		for (int i = 0; i < dan; i++) {
-			System.out.print(i + ":");
-			// *記入のfor文開始
-			for (int j = 0; j < i * i ; j++) {
-				System.out.print("*");
-			}
-			System.out.println("");
-		}
-	}
+    public static void main(String[] args) {
+        // 段数の限度指定
+        int dan = 10;
+        // 段数表示のfor文開始
+        for (int i = 0; i < dan; i++) {
+            System.out.print(i + ":");
+            // *記入のfor文開始
+            for (int j = 0; j < i * i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 
 }
