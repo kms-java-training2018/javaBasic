35fbf8a13a4c08ebdaef5d09be3b3f60c7f0152e
goto-kazuki <goto-kazuki@KMS2013-60.kms.local>
2018-05-14 15:17:35 +0900

    課題6_1提出(後藤)

---
+++ src/javatraining/BinaryDigits1.java
@@ -0,0 +1,17 @@
+package javatraining;
+
+public class BinaryDigits1 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        int binaryDigits[] = new int [5];
+        binaryDigits[0] = 1;
+        binaryDigits[1] = 2;
+        binaryDigits[2] = 4;
+        binaryDigits[3] = 8;
+        binaryDigits[4] = 16;
+
+        System.out.println("3つめの値は" + binaryDigits[2] + "です。");
+    }
+
+}
