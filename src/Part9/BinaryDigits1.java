0e72d62e2698db5a3923a843d6d03bb431cec1d6
yamagishi-toko <yamagishi-toko@KMS2013-78.kms.local>
2018-05-14 15:37:32 +0900

    BinaryDigits1提出

---
+++ src/Part9/BinaryDigits1.java
@@ -0,0 +1,18 @@
+package Part9;
+
+public class BinaryDigits1 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+
+        int binaryDigits[] = new int [5];
+        binaryDigits[0] = 1;
+        binaryDigits[1] = 2;
+        binaryDigits[2] = 4;
+        binaryDigits[3] = 8;
+        binaryDigits[4] = 16;
+
+    System.out.println("3つ目の値は" + binaryDigits[2] + "です。");
+    }
+
+}
