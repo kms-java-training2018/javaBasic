640aca3dab64b623ce5f3ceca42817f98170559f
takatsuka-akihito <takatsuka-akihito@KMS2013-69.kms.local>
2018-05-14 15:20:54 +0900

    BinaryDigits1です。

---
+++ src/javatraining/BinaryDigits1.java
@@ -0,0 +1,20 @@
+package javatraining;
+
+public class BinaryDigits1 {
+
+	public static void main(String[] args) {
+		//配列
+		int []BinaryDigits;
+		//配列
+		BinaryDigits = new int [5];
+		//要素
+		BinaryDigits[0] = 1;
+		BinaryDigits[1] = 2;
+		BinaryDigits[2] = 4;
+		BinaryDigits[3] = 8;
+		BinaryDigits[4] = 16;
+		//表示
+		System.out.println("3つ目の値は" + BinaryDigits[2] + "です。");
+	}
+
+}
