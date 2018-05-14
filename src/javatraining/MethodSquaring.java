396d1b9de3661cad67184ee3a6b3d15b0c033766
hanawa-tomonori <hanawa-tomonori@KMS2013-47.kms.local>
2018-05-14 11:50:07 +0900

    8章練習問題-MethodSquaring

---
+++ src/javatraining/MethodSquaring.java
@@ -0,0 +1,29 @@
+package javatraining;
+
+public class MethodSquaring {
+
+    public static void main(String[] args) {
+        // 引数とする数字を格納する変数を宣言
+        int p;
+        int q;
+        // 引数を２乗し結果を返すメソッドを呼び出す
+        p = 2; // 元の数値の決定
+        q = squaring(p);
+
+        // 結果を出力
+        System.out.println(p + "の2乗は、" + q + "です。");
+    }
+
+    /** 引数を2乗し、結果を返すメソッド **/
+    public static int squaring(int n) {
+        // 計算して結果を返す
+
+        int y = 1;
+        int z = 15; // 累乗値の決定
+        for (int x = 0; x < z; x++) {
+            y = n * y;
+        }
+        return y;
+    }
+
+}
