f3ebec804c674d9682f773b4d3c0851803840ddb
hanawa-tomonori <hanawa-tomonori@KMS2013-47.kms.local>
2018-05-15 13:10:41 +0900

    練習問題9-6"SelectGreeting"の修正をしました。

--- src/javatraining/SelectGreeting.java
+++ src/javatraining/SelectGreeting.java
@@ -16,7 +16,7 @@ public class SelectGreeting {
         }
         int num = Integer.parseInt(args[0]);
         if(0 <= num && num < message.length) {
-            System.out.println("message[num]");
+            System.out.println(message[num]);
         } else {
             System.out.println("番号は0～" + 2 + "の範囲で指定してください。");
         }
