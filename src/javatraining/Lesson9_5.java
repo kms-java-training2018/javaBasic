2fac2dc2e56e433dc8df15e1c0bf6588cb5bf72b
goto-kazuki <goto-kazuki@KMS2013-60.kms.local>
2018-05-15 10:01:24 +0900

    問題9-5と9-6提出

---
+++ src/javatraining/Lesson9_5.java
@@ -0,0 +1,26 @@
+package javatraining;
+
+public class Lesson9_5 {
+
+    //問題9-5  どうしてエラーになるのか
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        double[] data;
+
+        //↓new double[]内が5になっていたため、計算ができていなかった。
+        data = new double[6];
+        data[0] = 3.1;
+        data[1] = 4.1;
+        data[2] = 5.9;
+        data[3] = 2.6;
+        data[4] = 5.3;
+        data[5] = 9.7;
+        double sum = 0.0;
+        for (int i = 0; i < data.length; i++) {
+            sum += data[i];
+        }
+        System.out.println("sum=" + sum);
+    }
+
+}
