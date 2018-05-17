99cf8456c3e3d3827727679ee786770340594a9b
yamagishi-toko <yamagishi-toko@KMS2013-78.kms.local>
2018-05-17 20:32:58 +0900

    問題12-5

--- src/javatraining/Part12/PlaceRectangle.java
+++ src/javatraining/Part12/PlaceRectangle.java
@@ -1,30 +1,32 @@
 package javatraining.Part12;
 
-import javatraining.Part11.Rectangle;
+public class PlaceRectangle{
 
-public class PlaceRectangle extends Rectangle {
-
+    Rectangle r;
     //フィールド(位置)
     int x;
     int y;
 
     //引数なしコンストラクタ
     PlaceRectangle(){
-         setLocation(0,0);
+        r = new Rectangle (2,8);
+        setLocation(0,0);
     }
 
     //位置付きコンストラクタ
     PlaceRectangle(int x, int y){
+        r = new Rectangle (2,8);
         this.x = x;
         this.y = y;
     }
 
     //位置と大きさ付きコンストラクタ
     PlaceRectangle(int x, int y, int width, int height){
+        r = new Rectangle (2,8);
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        r.width = width;
+        r.height = height;
     }
 
     //位置を変更するメソッド
@@ -40,6 +42,7 @@ public class PlaceRectangle extends Rectangle {
     }
 
     public static void main(String[] args) {
+
         PlaceRectangle pr = new PlaceRectangle (12, 34, 123, 45);
         String result = pr.toString();
         System.out.println(result);
