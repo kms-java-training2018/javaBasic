d6d80bb1ff087b630b86979d1832c9af6f022889
iijima-naoyuki <iijima-naoyuki@KMS2014-06.kms.local>
2018-05-17 10:00:13 +0900

    提出できていなかった9章の練習問題の提出

---
+++ src/javatraining/SerectGreeting.java
@@ -0,0 +1,35 @@
+package javatraining;
+
+/**
+ * 教科書　上　p.238
+ * 問題9-6
+ * 「番号指定の挨拶プログラムを完成させましょう」
+ * */
+public class SerectGreeting {
+
+    public static void main(String[] args) {
+        String[] message = new String[3]; //messageに3個の配列を用意(???)
+        //表示させる内容を配列に入れる。(???)
+        message[0] = "おはよう！";
+        message[1] = "こんにちは！";
+        message[2] = "こんばんは！";
+
+        if (args.length != 1) { //番号の指定がない場合 or 複数の番号が指定された場合。
+            System.out.println("使い方： java selectGreeting 番号");
+            System.exit(0);
+        }
+        int num = Integer.parseInt(args[0]); //指定された番号をint型変数numに代入。(???)
+        /*
+         * 指定された番号が0以上要素の数以下の場合、その番号のメッセージを出力。
+         * それ以外の場合は範囲内での指定を促す。
+         * */
+        if (0 <= num && num < message.length) {
+            System.out.println(message[num]);
+        } else {
+            //???→(message.length - 1)
+            System.out.println("番号は0～" + (message.length - 1) + "の範囲で指定してください。");
+        }
+
+    }
+
+}
