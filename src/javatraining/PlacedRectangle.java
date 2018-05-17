b424cf85766439b2f79a0d7dee330641bad8791c
hanawa-tomonori <hanawa-tomonori@KMS2013-47.kms.local>
2018-05-17 18:46:14 +0900

    問題12-5の提出をいたします

--- src/javatraining/PlacedRectangle.java
+++ src/javatraining/PlacedRectangle.java
@@ -5,47 +5,38 @@ package javatraining;
  * 花輪朋典
  *
  * -------------------------------------------------------------------------------
- * 問題12-4
- * 次の機能を持つPlacedRectangleを、Rectangleのサブクラスとして宣言してください。
- *
- *   ・位置を表すint型のフィールドx, yを持つ
- *   ・3つのコンストラクタを持つ
- *       (1)引数なし
- *       (2)位置付き
- *       (3)位置と大きさ付き
- *   ・位置を変更するメソッドsetLocationを持つ
- *   ・標準的な文字列表現を返すメソッドtoStringを持つ
+ * 問題12-5
+ * 問題12-4のPlacedRectangleを、Rectangleのサブクラスとしてではなく
+ * Rectangle型のインスタンス変数を持つクラスとして宣言してください。
  * -------------------------------------------------------------------------------
  *
- *
- * 回答を読みながら、全体的な修正をしました。
- *  ・superの追加
- *  ・コメントの追加
- *  ・Overrideの追加
- *  ・return文の変更
- *  ・引数の変更
+ * 12-4と比べ大きな変更はありませんが、Rectangle型の変数rectangleを宣言し
+ * Rectangleクラスで入力された変数widthと変数heightを呼びだしました。
  *
  */
 
 
 
-public class PlacedRectangle extends Rectangle {
+public class PlacedRectangle{
     // フィールドの宣言
+    Rectangle rectangle;
     int x;
     int y;
 
     // コンストラクタ
     // 引数なし
     PlacedRectangle(){
+        rectangle = new Rectangle();
         setLocation(0, 0);
     }
     // 位置付き
     PlacedRectangle(int x, int y){
+        rectangle = new Rectangle();
         setLocation(x, y);
     }
     // 位置と大きさ付き
     PlacedRectangle(int x, int y, int width, int height){
-        super(width, height);
+        rectangle = new Rectangle(width, height);
         setLocation(x, y);
     }
     // 位置を変更するメソッド
@@ -55,9 +46,8 @@ public class PlacedRectangle extends Rectangle {
 
     }
     // 文字列表現を返すメソッド
-    // @Overrideの追記、setStringから、toStringへ変更
     @Override
     public String toString() {
-        return "[ (" + x + ", " + y + ") " + super.toString()+" ]";
+        return "[ (" + x + ", " + y + ") " + rectangle +" ]";
     }
 }
