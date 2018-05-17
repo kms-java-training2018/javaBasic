af2643ba006ec4cce304bdf87ad2417f98b4b8b5
hanawa-tomonori <hanawa-tomonori@KMS2013-47.kms.local>
2018-05-17 16:45:03 +0900

    (修正)先ほど提出をしましたが、コメントがなかったためコメントを追加いたしました。

--- src/javatraining/PlacedRectangle.java
+++ src/javatraining/PlacedRectangle.java
@@ -3,42 +3,42 @@ package javatraining;
 
 /*
  * 花輪朋典
- * 問題12-4
- * 次の機能を持つPlacedRectangleを、Rectangleのサブクラスとして宣言してください。
- *   ・位置を表すint型のフィールドx, yを持つ
- *   ・3つのコンストラクタを持つ
- *     （１）引数なし
- *     （２）位置付き
- *     （３）位置と大きさ付き
- *   ・位置を変更するメソッドsetLocationを持つ
- *   ・標準的な文字列表現を返すメソッドtoStringを持つ
+ *
+ * 問題12-5
+ * 問題12-4のPlaceRectangleを、Rectangleのサブクラスとしてではなく、
+ * Rectangle型のインスタンス変数を持つクラスとして宣言してください。
+ *
+ *
  */
 
 
 
 public class PlacedRectangle extends Rectangle {
+    // フィールドの宣言
     int x;
     int y;
 
+    // コンストラクタ
+    // 引数なし
     PlacedRectangle(){
         setLocation(0, 0, 0, 0);
     }
-
+    // 位置付き
     PlacedRectangle(int x, int y){
         setLocation(x, y, 0, 0);
     }
-
+    // 位置と大きさ付き
     PlacedRectangle(int x, int y, int width, int height){
         setLocation(x, y, width, height);
     }
-
+    // 位置を変更するメソッド
     public void setLocation(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
-
+    // 文字列表現を返すメソッド
     final public String setString() {
         return "[ (" + x + ", " + y + ") [" + width + ", " + height + "]]";
     }
