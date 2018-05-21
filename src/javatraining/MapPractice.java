237e2a8ac6eb1ee6d81b39958ec5dddd07a7a0d9
ishii-masahiro <ishii-masahiro@KMS2013-113.kms.local>
2018-05-21 14:45:51 +0900

    課題MapPracticeを変更します。

--- src/javatraining/MapPractice.java
+++ src/javatraining/MapPractice.java
@@ -29,29 +29,24 @@ public class MapPractice {
         		System.out.println("翻訳したい日本語を入力してください。");
         
         		// 入力された日本語を取得
-        		String line1 = reader.readLine();
+        		String line = reader.readLine();
 			
         		// Map内に対応する英語があるか判断
-        		if (map.get(line1) == null) {
+        		if (map.get(line) == null) {
         			// 対応する英語がない場合
         			System.out.println("対応する英語がありません");
         		
         		} else {
         			// 対応する英語がある場合
-        			System.out.println("「" + line1 + "」の英語は「" + map.get(line1) + "」");
+        			System.out.println("「" + line + "」の英語は「" + map.get(line) + "」");
         		}
         		
     			// 継続するか判断
-        		System.out.println("続けて翻訳する場合 0 を入力してください。");
+        		System.out.println("続けて翻訳する場合 「0」 を入力してください。");
         		
     			// 継続する場合0を入力する
-        		String line2 = reader.readLine();
-        		int judge = Integer.parseInt(line2);
-        		
-    			// 0以外が入力された場合終了
-        		if (judge != 0) {
-        			loop = 1;
-        		}
+        		line = reader.readLine();
+        		loop = Integer.parseInt(line);
         	}
         	
 			// 終了する
